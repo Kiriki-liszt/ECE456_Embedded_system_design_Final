90161be046753f9bab7ab802493dd71f17e74abf
Kiriki-liszt <kirikiaris@gmail.com>
2022-06-18 08:08:22 +0900

    028 - register

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,10 +2,10 @@ LOCAL_PATH:=$(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS += -fPIE -mfloat-abi=softfp -mfpu=neon -fopenmp -g
-LOCAL_LDFLAGS += -fPIE -fopenmp -static-openmp # -pie
+LOCAL_CFLAGS += -fPIE -g
+LOCAL_LDFLAGS += -fPIE # -pie
 
-LOCAL_MODULE:=project_basis
+LOCAL_MODULE:=recognition_seq
 
 LOCAL_SRC_FILES:=recognition_seq.c main.c
 
