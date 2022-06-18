ff119b3e1f060671ab71ec53fa0b4984cb7d40be
Kiriki-liszt <kirikiaris@gmail.com>
2022-06-18 21:40:31 +0900

    029 - loop jamming

--- jni/recognition_seq.c
+++ jni/recognition_seq.c
@@ -9,7 +9,7 @@
 
 void recognition(float * images, float * network, int depth, int size, int * labels, float * confidences)
 {
-	register int 	i, j, x, y;
+	register int 	i, j, x, y, size_x;
 			float 	*hidden_layers, *temp, **weights, **biases;
 
 	hidden_layers	= (float *)malloc(sizeof(float) * size * depth);
@@ -19,78 +19,89 @@ void recognition(float * images, float * network, int depth, int size, int * lab
 	// Set pointers for weights and biases
 	// 1. Input layer
 	weights[0] = network;
-	biases[0] = weights[0] + size * IMG_SIZE;
+	biases[0]  = weights[0] + size * IMG_SIZE;
 
 	// 2. Hidden layers
-	for(i = 1; i < depth; i++)
-	{
-		weights[i] = network + (size * IMG_SIZE + size) + (size * size + size) * (i-1);
-		biases[i] = weights[i] + size * size;
-	}
+	// layer 1
+	weights[1] = network + (size * IMG_SIZE + size);
+	biases[1]  = weights[1] + size * size;
+	// layer 2
+	weights[2] = network + (size * IMG_SIZE + size) + (size * size + size);
+	biases[2]  = weights[2] + size * size;
 
 	// 3. Output layer
 	weights[depth] = weights[depth - 1] + size * size + size;
-	biases[depth] = weights[depth] + DIGIT_COUNT * size;
+	biases[depth]  = weights[depth] + DIGIT_COUNT * size;
+
+	float * input = images;
 
 	// Recognize numbers
 	for(i = 0; i < IMG_COUNT; i++)
 	{
-		float * input = images + IMG_SIZE * i;
 		float output[DIGIT_COUNT];
 
 		// From the input layer to the first hidden layer
+		size_x = 0;
 		for(x = 0; x < size; x++)
 		{
 			float sum = 0;
 			for(y = 0; y < IMG_SIZE; y++)
 			{
-				sum += input[y] * weights[0][IMG_SIZE * x + y];
+				sum += input[y] * weights[0][size_x + y];
 			}
 			sum += biases[0][x];
 			hidden_layers[x] = sigmoid(sum);
+			size_x += IMG_SIZE;
 		}
 
 		// Between hidden layers
-		for(j = 1; j < depth; j++)
+		// layer 1
+		size_x = 0;
+		for(x = 0; x < size; x++)
 		{
-			for(x = 0; x < size; x++)
+			float sum = 0;
+			for(y = 0; y < size; y++)
 			{
-				float sum = 0;
-				for(y = 0; y < size; y++)
-				{
-					sum += hidden_layers[size * (j-1) + y] * weights[j][size * x + y];
-				}
-				sum += biases[j][x];
-				hidden_layers[size * j + x] = sigmoid(sum);
+				sum += hidden_layers[y] * weights[1][size_x + y];
 			}
+			sum += biases[1][x];
+			hidden_layers[size + x] = sigmoid(sum);
+			size_x += size;
 		}
+		// layer 2
+		size_x = 0;
+		for(x = 0; x < size; x++)
+		{
+			float sum = 0;
+			for(y = 0; y < size; y++)
+			{
+				sum += hidden_layers[size + y] * weights[2][size_x + y];
+			}
+			sum += biases[2][x];
+			hidden_layers[size * 2 + x] = sigmoid(sum);
+			size_x += size;
+		}
+
 		
 		// From the last hidden layer to the output layer
+		size_x = 0;
 		for(x = 0; x < DIGIT_COUNT; x++)
 		{
 			float sum = 0;
 			for(y = 0; y < size; y++)
 			{
-				sum += hidden_layers[size * (depth-1) + y] * weights[depth][size * x + y];
+				sum += hidden_layers[size * (depth-1) + y] * weights[depth][size_x + y];
 			}
 			sum += biases[depth][x];
 			output[x] = sigmoid(sum);
-		}
+			size_x += size;
 
-		// Find the answer
-		float max = 0;
-		int label = 0;
-		for(x = 0; x < DIGIT_COUNT; x++)
-		{
-			if(output[x] > max)
+			if(output[x] > confidences[i])
 			{
-				label = x;
-				max = output[x];
+				labels[i]= x;
+				confidences[i] = output[x];
 			}
 		}
-
-		// Store the result
-		confidences[i] = max;
-		labels[i] = label;
+		input += IMG_SIZE;		// reduction in stregnth
 	}
 }
