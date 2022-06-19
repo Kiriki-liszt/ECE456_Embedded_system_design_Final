f4e1a38e660f13cd4d5e300aa5938f8a7d0734ee
Kiriki-liszt <kirikiaris@gmail.com>
2022-06-19 13:43:34 +0900

    032-vku

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:=$(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS += -fPIE -mfloat-abi=softfp -mfpu=neon -g
+LOCAL_CFLAGS += -fPIE -mfloat-abi=soft -g # -mfloat-abi=softfp   //fp -mfpu=neon
 LOCAL_LDFLAGS += -fPIE # -pie
 
 LOCAL_MODULE:=recognition_seq
